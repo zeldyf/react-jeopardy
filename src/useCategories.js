fab8750e6fd5eda825a00d77ea8e24d66c401c7a
Zeldy Friedmana <zeldyfriedman@gmail.com>
2023-08-27 17:44:14 -0400

    fixed loading time

--- src/useCategories.js
+++ src/useCategories.js
@@ -1,11 +1,13 @@
 import fetchCategories from "./fetchCategories";
+
 import { useQuery } from "@tanstack/react-query";
 
+const params = {
+    count: 100,
+    offset: Math.floor(Math.random() * (500 - 1) + 1),
+};
+    
 export default function useCategories (){
-    const params = {
-        count: 100,
-        offset: Math.floor(Math.random() * (500 - 1) + 1),
-    };    
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const results = useQuery(["category", params], fetchCategories);
 
